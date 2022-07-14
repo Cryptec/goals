be2f6ef8fe3481529571cee8a39141beb689add3
Cryptec <neupi92@web.de>
2022-07-14 13:25:49 +0200

    initial commit

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import GoalElement from './Components/Goals';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='GoalWrapper'>
+        <GoalElement />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/Components/Goals.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+import '../css/Goals.css'
+
+class GoalElement extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: ''
+        };
+    }
+
+    render () {
+        return (
+            <div>
+                <div className='Goalfield'>
+                   <p>1 Jahr</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default GoalElement
